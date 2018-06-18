7c7f2745ff99f077273dd356a5a79ee16c9ed14b
Colm Tuite <colmtuite@gmail.com>
2018-06-18 04:11:37 +0100

    Update spacing scale from 8dp to 5dp

--- src/theme/Spacing.js
+++ src/theme/Spacing.js
@@ -1,9 +1,9 @@
+export const SPACING_100 = '.5rem';
+export const SPACING_200 = '1rem';
+export const SPACING_300 = '1.5rem';
+export const SPACING_400 = '2rem';
+export const SPACING_500 = '2.5rem';
+export const SPACING_600 = '3.5rem';
+export const SPACING_700 = '4.5rem';
+export const SPACING_800 = '6.5rem';
 export const SPACING_900 = '8rem';
-export const SPACING_800 = '6.4rem';
-export const SPACING_700 = '4.8rem';
-export const SPACING_600 = '4rem';
-export const SPACING_500 = '3.2rem';
-export const SPACING_400 = '2.4rem';
-export const SPACING_300 = '1.6rem';
-export const SPACING_200 = '.8rem';
-export const SPACING_100 = '.4rem';
