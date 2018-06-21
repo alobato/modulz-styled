fa5a36797fb0c7dca850de184307de54f9c40556
Colm Tuite <colmtuite@gmail.com>
2018-06-21 13:43:25 +0100

    Add corrective margins to Text component

--- src/components/Text.js
+++ src/components/Text.js
@@ -5,29 +5,48 @@ import * as theme from './../theme/';
 const Text = styled.p`
   color: ${theme.GRAY_900};
 
-  ${props => props.size1 && css`
+  ${p => p.size1 && css`
     font-size: ${theme.FONTSIZE_200};
   `}
-  ${props => props.size2 && css`
+  
+  ${p => p.size2 && css`
     font-size: ${theme.FONTSIZE_300};
   `}
-  ${props => props.size3 && css`
+  
+  ${p => p.size3 && css`
     font-size: ${theme.FONTSIZE_400};
+    letter-spacing: -.001em;
+    margin-left: -.05em;
   `}
-  ${props => props.size4 && css`
+
+  ${p => p.size4 && css`
     font-size: ${theme.FONTSIZE_500};
+    letter-spacing: -.005em;
+    margin-left: -.06em;
   `}
-  ${props => props.size5 && css`
+
+  ${p => p.size5 && css`
     font-size: ${theme.FONTSIZE_600};
+    letter-spacing: -.012em;
+    margin-left: -.075em;
   `}
-  ${props => props.size6 && css`
+
+  ${p => p.size6 && css`
     font-size: ${theme.FONTSIZE_700};
+    letter-spacing: -.028em;
+    margin-left: -.085em;
   `}
-  ${props => props.size7 && css`
+
+  ${p => p.size7 && css`
     font-size: ${theme.FONTSIZE_800};
+    letter-spacing: -.038em;
+    margin-left: -.088em;
   `}
-  ${props => props.size8 && css`
+
+  ${p => p.size8 && css`
     font-size: ${theme.FONTSIZE_900};
+    letter-spacing: -.042em;
+    margin-left: -.09em;
   `}
 `;
 
