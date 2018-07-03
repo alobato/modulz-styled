2dc8f17076ffcb6eb7341fb5a71bdf5462cad614
Colm Tuite <colmtuite@gmail.com>
2018-07-03 12:31:52 +0100

    Composer progress

--- src/Composer.jsx
+++ src/Composer.jsx
@@ -33,12 +33,6 @@ class Composer extends Component {
             bcgray300
             fs0
             style={{width: '300px'}}>
-            <Heading
-              size2
-              gray600
-              mb6>
-              Layers
-            </Heading>
             <List>
               <LinkItem>
                 <Box
@@ -104,28 +98,14 @@ class Composer extends Component {
           </Box>
           <Box
             fg1>
-            <Heading
-              size2
-              gray600
-              mb6>
-              Devices
-            </Heading>
           </Box>
           <Box
             bl
             bcgray300
             fs0
-            style={{width: '210px'}}>
-            <Heading
-              size2
-              gray600
-              mb6>
-              Styles
-            </Heading>
+            style={{width: '220px'}}>
             <Box
-              p2
-              bt
-              bcgray300>
+              p3>
               <Flexbox
                 mb2
                 ai_center>
@@ -133,6 +113,7 @@ class Composer extends Component {
                   mr2>
                   <Text
                     size1
+                    gray600
                     right>
                     Width
                   </Text>
@@ -140,11 +121,11 @@ class Composer extends Component {
                 <Box
                   mr2>
                   <Input
-                    value="Auto"/>
+                    value="0"/>
                 </Box>
                 <Box>
                   <Input
-                    value="Auto"/>
+                    value="0"/>
                 </Box>
               </Flexbox>
               <Flexbox
@@ -154,6 +135,7 @@ class Composer extends Component {
                   mr2>
                   <Text
                     size1
+                    gray600
                     right>
                     Min
                   </Text>
@@ -161,11 +143,11 @@ class Composer extends Component {
                 <Box
                   mr2>
                   <Input
-                    value="Auto"/>
+                    value="0"/>
                 </Box>
                 <Box>
                   <Input
-                    value="Auto"/>
+                    value="0"/>
                 </Box>
               </Flexbox>
               <Flexbox
@@ -174,6 +156,7 @@ class Composer extends Component {
                   mr2>
                   <Text
                     size1
+                    gray600
                     right>
                     Max
                   </Text>
@@ -181,21 +164,87 @@ class Composer extends Component {
                 <Box
                   mr2>
                   <Input
-                    value="Auto"/>
+                    value="0"/>
                 </Box>
                 <Box>
                   <Input
-                    value="Auto"/>
+                    value="0"/>
                 </Box>
               </Flexbox>
             </Box>
 
             <Box
-              p2
+              p3
+              bt
+              bcgray300>
+              <Box
+                mb2>
+                <Text
+                  size1
+                  gray600>
+                  Margin
+                </Text>
+              </Box>
+              <Flexbox
+                mb3
+                ai_center>
+                <Box
+                  mr2>
+                  <Input
+                    value="0"/>
+                </Box>
+                <Box
+                  mr2>
+                  <Input
+                    value="0"/>
+                </Box>
+                <Box
+                  mr2>
+                  <Input
+                    value="0"/>
+                </Box>
+                <Box>
+                  <Input
+                    value="0"/>
+                </Box>
+              </Flexbox>
+              <Box
+                mb2>
+                <Text
+                  size1
+                  gray600>
+                  Padding
+                </Text>
+              </Box>
+              <Flexbox
+                ai_center>
+                <Box
+                  mr2>
+                  <Input
+                    value="0"/>
+                </Box>
+                <Box
+                  mr2>
+                  <Input
+                    value="0"/>
+                </Box>
+                <Box
+                  mr2>
+                  <Input
+                    value="0"/>
+                </Box>
+                <Box>
+                  <Input
+                    value="0"/>
+                </Box>
+              </Flexbox>
+            </Box>
+
+            <Box
+              p3
               bt
               bcgray300>
               <Flexbox
-                mb2
                 ai_center>
                 <Box
                   mr2>
@@ -215,6 +264,90 @@ class Composer extends Component {
                 <Box>
                   <Input
                     value="100"/>
+                </Box>
+              </Flexbox>
+            </Box>
+
+            <Box
+              p3
+              bt
+              bcgray300>
+              <Box
+                mb2>
+                <Box>
+                  <Flexbox>
+                    <Box
+                      mr2>
+                      <Select>
+                        <option
+                          value="100">
+                          100
+                        </option>
+                        <option
+                          value="200">
+                          200
+                        </option>
+                        <option
+                          value="300">
+                          300
+                        </option>
+                      </Select>
+                    </Box>
+                    <Box>
+                      <Input />
+                    </Box>
+                  </Flexbox>
+                </Box>
+              </Box>
+              <Flexbox
+                mb2
+                ai_center
+                jc_spacebetween>
+                <Box
+                  mr2>
+                  <Text
+                    gray600
+                    size1
+                    right>
+                    Alignment
+                  </Text>
+                </Box>
+                <Box>
+                  <Flexbox>
+                    <ToggleButton
+                      name="text_alignment"
+                      checked={true}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none" stroke="currentColor">
+                        <path d="M0 0H9" transform="translate(4 16.5)" />
+                        <path d="M0 0H17" transform="translate(4 12.5)" />
+                        <path d="M0 0H9" transform="translate(4 8.5)" />
+                      </svg>
+                    </ToggleButton>
+                    <ToggleButton
+                      name="text_alignment">
+                      <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
+                        <path d="M0 0H9" transform="translate(8 16.5)"/>
+                        <path d="M0 0H17" transform="translate(4 12.5)"/>
+                        <path d="M0 0H9" transform="translate(8 8.5)"/>
+                      </svg>
+                    </ToggleButton>
+                    <ToggleButton
+                      name="text_alignment">
+                      <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
+                        <path d="M0 0H9" transform="translate(12 16.5)"/>
+                        <path d="M0 0H17" transform="translate(4 12.5)"/>
+                        <path d="M0 0H9" transform="translate(12 8.5)"/>
+                      </svg>
+                    </ToggleButton>
+                    <ToggleButton
+                      name="text_alignment">
+                      <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
+                        <path d="M0 0H17" transform="translate(4 16.5)"/>
+                        <path d="M0 0H17" transform="translate(4 12.5)"/>
+                        <path d="M0 0H17" transform="translate(4 8.5)"/>
+                      </svg>
+                    </ToggleButton>
+                  </Flexbox>
                 </Box>
               </Flexbox>
             </Box>
