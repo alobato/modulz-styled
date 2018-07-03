7d7ebbf45aca3b68f4d7aa0d7573989aad345ecb
Colm Tuite <colmtuite@gmail.com>
2018-07-03 12:33:23 +0100

    Add default padding to List

--- src/components/List.js
+++ src/components/List.js
@@ -3,6 +3,7 @@ import * as theme from './../theme/';
 
 const List = styled.ul`
   list-style: none;
+  padding: ${theme.SPACING_200} 0;
 
   ${props => props.rounded && css`
     border-radius: ${theme.BORDERRADIUS_200};
