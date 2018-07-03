fde702e7cc5c9c5fe747c595a32ba47143277606
Colm Tuite <colmtuite@gmail.com>
2018-07-03 08:03:11 +0100

    Add Image component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Text from './components/Text.js';
 import Heading from './components/Heading.js';
 import Divider from './components/Divider.js';
 import Grid from './components/Grid.js';
+import Image from './components/Image.js';
 import Badge from './components/Badge.js';
 import Input from './components/Input.js';
 import CheckboxButton from './components/CheckboxButton.js';
@@ -429,6 +430,17 @@ class App extends Component {
                 Start designing
               </LinesButton>
             </Flexbox>
+          </Container>
+        </Section>
+
+        <Section size3>
+          <Container size3>
+            <Heading size2 gray600 mb6>
+              Image
+            </Heading>
+            <Image
+              src="https://images.unsplash.com/photo-1530484490308-d5f6e103e149?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=db3be4d42ecb40d27750b3e13d05d306&auto=format&fit=crop&w=1650&q=80">
+            </Image>
           </Container>
         </Section>
 
